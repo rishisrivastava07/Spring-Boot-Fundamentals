1bb3c0da0bc7341b72b35f81f16cb361d246b42e
Rishi Srivastava <144473657+rishisrivastava07@users.noreply.github.com>
2025-02-22 12:15:31 +0530

    Delete journalApp directory

--- journalApp/src/main/java/com/rishi/journalApp/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rishi.journalApp.repository;
-
-import com.rishi.journalApp.entity.User;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, ObjectId> {
-    User findByUsername(String username);
-}
